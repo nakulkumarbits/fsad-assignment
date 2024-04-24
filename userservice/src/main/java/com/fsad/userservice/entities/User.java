d5d83754ced461f15bbbb8719752631bf2b3e61b
nakul <nakulkumar14.09@gmail.com>
2024-04-25 03:09:26 +0530

    Backend - ID generation updated to IDENTITY from AUTO

--- userservice/src/main/java/com/fsad/userservice/entities/User.java
+++ userservice/src/main/java/com/fsad/userservice/entities/User.java
@@ -16,7 +16,7 @@ import java.util.StringJoiner;
 @Getter
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(nullable = false)
